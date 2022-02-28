28ec9256582e30cfa48c1b93e23d64cdfa89af4f
hasanussafa <45102174+hasanussafa@users.noreply.github.com>
2022-02-27 22:32:07 -0500

    Add files via upload
    Updated index file

--- index.js
+++ index.js
@@ -8,21 +8,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-//import typedefs and resolvers
 const TypeDefs = require('./schema')
 const Resolvers = require('./resolvers')
-
-//import ApolloServer
 const { ApolloServer } = require('apollo-server-express')
-
-//Store sensitive information to env variables
 const dotenv = require('dotenv');
+
 dotenv.config();
-
-//mongoDB Atlas Connection String
 const mongodb_atlas_url = process.env.MONGODB_URL;
-
-//TODO - Replace you Connection String here
 mongoose.connect(mongodb_atlas_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -38,17 +30,12 @@ const server = new ApolloServer({
   resolvers: Resolvers.resolvers
 })
 
-//Define Express Server
 const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use('*', cors());
-
-//Add Express app as middleware to Apollo Server
 server.applyMiddleware({app})
 
-//console.log(server)
-
-//Start listen 
+//Run localhost
 app.listen({ port: process.env.PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`));
